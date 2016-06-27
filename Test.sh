24f48e20bdd436d0c2ab9668514feb7e5a5ce12c
Paul Darcey <pdarcey@gmail.com>
2016-06-27 13:17:16 +1000

    setDefaults, processLaunchArguments

---
+++ Test.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/env xcrun swift
+
+import Foundation
+
+print("This is a swift script!")
+print("Arguments: \(Process.arguments)")
--- videoDownloader.swift
+++ videoDownloader.swift
@@ -7,51 +7,65 @@
 
 import Foundation
 
-MARK: Launch Arguments
-// Processing launch arguments
-// http://ericasadun.com/2014/06/12/swift-at-the-command-line/
+setDefaults()
+processLaunchArguments()
+getDownloads()
 
-let arguments = Process.arguments as [String]
-let dashedArguments = arguments.filter({$0.hasPrefix("-")})
+func setDefaults {
+	// Default to get all HD videos from this year, no PDFs, and save to current directory
+	var resolution = .HD
+	var getAll = true
+	var getVideo = true
+	var getPDF = false
+	var year = 2016
+	var saveToDirectory = "" // TODO: set proper default
+}
 
-for argument : NSString in dashedArguments {
-    let key = argument.substringFromIndex(1)
-    let value : AnyObject? = NSUserDefaults.standardUserDefaults().valueForKey(key)
-    let valueString = value as? String
-    // print("    \(argument) \(value)")
-    
-    switch argument {
-    	case "-d":
-			if let directory = valueString {
-				directoryToSaveTo = directory
-			}
-    
-		case "-f" where valueString == "HD":
-			resolution = .HD
-	
-		case "--nopdf":
-			getPDF = false
-	
-		case "--pdfonly":
-			getPDF = true
-			getVideo = false
-	
-		case "-a":
-			getAll = true
+func processLaunchArguments() {
+	// MARK: Launch Arguments
+	// Processing launch arguments
+	// http://ericasadun.com/2014/06/12/swift-at-the-command-line/
+	let arguments = Process.arguments
 
-		case "-s"
-			sessionIDs = (valueString?.componentsSeparatedByString(","))!
-			getAll = false
-			print("Downloading for sessions: \(sessionIds)")
+	for argument in arguments {    
+		switch argument {
+			case "-d":
+				if let directory = valueString {
+					directoryToSaveTo = directory
+				}
 	
-		case "-y":
-			if let yearString = valueString {
-				year = yearString
-			}
+			case "-f" where valueString == "HD":
+				resolution = .HD
+	
+			case "--nopdf":
+				getPDF = false
+	
+			case "--pdfonly":
+				getPDF = true
+				getVideo = false
+	
+			case "-a":
+				getAll = true
+
+			case "-s"
+				sessionIDs = (valueString?.componentsSeparatedByString(","))!
+				getAll = false
+				print("Downloading for sessions: \(sessionIds)")
+	
+			case "-y":
+				if let yearString = valueString {
+					year = yearString
+				}
 		
-		default:
-			displaySyntaxError()
+			default:
+				displaySyntaxError()
+		}
 	}
 }
 
+func displaySyntaxError() {
+	    print("videoDownloader")
+        print("usage: videoDownloader.swift [-d directory] [-a] [-s ] [-pdf-only] [-help]\n")
+        exit(0)
+}
 
