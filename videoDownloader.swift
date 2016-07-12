299b78c83e9e16d7c7364eeaff31914ca7002563
Paul Darcey <pdarcey@gmail.com>
2016-07-12 12:19:02 +1000

    Refactored into classes/structs
    Refactored everything into logical classes/structs.
    Added downloader

--- videoDownloader.swift
+++ videoDownloader.swift
@@ -25,209 +25,250 @@ extension String {
     }
 }
 
-// Set default values
-// Default to get all HD videos from this year, no PDFs, and save to current directory
-var resolution : Resolution = .hd
-var getAll : Bool = true
-var getVideo : Bool = true
-var getPDF : Bool = false
-var year : Int = 2016
-var saveToDirectory : String = "~/Downloads"
-var sessionIDs : [String] = []
-var message : String = ""
-var userSetGetAll = false
-var userSetSessionList = false
+class Setup {
+    // Set default values
+    // Default to get all HD videos from this year, no PDFs, and save to current directory
+    var resolution : Resolution = .hd
+    var getAll : Bool = true
+    var getVideo : Bool = true
+    var getPDF : Bool = false
+    var year : Int = 2016
+    var saveToDirectory : String = "WWDCVideos"
+    var sessionIDs : [String] = []
+    var message : String = ""
+    var userSetGetAll = false
+    var userSetSessionList = false
 
-func processLaunchArguments() {
-    // Processing launch arguments
-    // http://ericasadun.com/2014/06/12/swift-at-the-command-line/
-    let arguments = Process.arguments
-    
-    var i = 0
-    
-    for argument in arguments {
-        i += 1
-        if argument.hasPrefix("-") {
-            switch argument {
-            case "-d":										// Nominate the directory to save downloads to. Default is ~/Downloads
-                if i <= arguments.count {
-                    let saveToDirectory = arguments[i] as String
-                    message = message + "\nDownloading to directory: \(saveToDirectory)"
-                } else {
-                    displaySyntaxError()
-                }
-                
-            case "-a":										// Get all sessions
-                userSetGetAll = true
-                if userSetSessionList {
-                    displaySyntaxError("*** You have set both the -a and -s options. These are mutually exclusive ***")
-                } else {
-                    getAll = true
-                    message = message + "\nGet all videos"
-                }
-                
-            case "-s":										// Nominate which sessions to download
-                userSetSessionList = true
-                if userSetGetAll {
-                    displaySyntaxError("*** You have set both the -a and -s options. These are mutually exclusive ***")
-                } else {
+    func processLaunchArguments() {
+        // Processing launch arguments
+        // http://ericasadun.com/2014/06/12/swift-at-the-command-line/
+        let arguments = Process.arguments
+        
+        var i = 0
+        
+        for argument in arguments {
+            i += 1
+            if argument.hasPrefix("-") {
+                switch argument {
+                case "-d":										// Nominate the directory to save downloads to. Default is ~/Downloads
                     if i <= arguments.count {
-                        var sessions = ""
-                        var j = i
-                        while j < arguments.count && !arguments[j].hasPrefix("-") {
-                            sessions = sessions + arguments[j] as String
-                            j += 1
-                        }
-                        sessionIDs = sessions.componentsSeparatedByString(",")
-                        getAll = false
-                        message = message + "Downloading \(sessionIDs.count) sessions: \(sessionIDs)"
+                        self.saveToDirectory = arguments[i] as String
+                        self.message = self.message + "\nDownloading to directory: \(self.saveToDirectory)"
                     } else {
                         displaySyntaxError()
                     }
-                }
-                
-            case "-sd":										// Get SD videos
-                resolution = .sd
-                message = message + "\nGet SD resolution"
-                
-            case "-hd":										// Get HD videos
-                resolution = .hd
-                message = message + "\nGet HD resolution"
-                
-            case "-nopdf":									// Don't get the associated PDFs
-                getPDF = false
-                message = message + "\nDon't get PDFs"
-                
-            case "-pdfonly":								// Only download the PDFs (and not the videos)
-                getPDF = true
-                getVideo = false
-                message = message + "\nOnly get PDFs"
-                
-            case "-y":										// Nominate the year
-                if i <= arguments.count {
-                    let year = Int(arguments[i])
-                    message = message + "\nGet downloads from \(year)"
                     
-                } else {
+                case "-a":										// Get all sessions
+                    self.userSetGetAll = true
+                    if self.userSetSessionList {
+                        displaySyntaxError("*** You have set both the -a and -s options. These are mutually exclusive ***")
+                    } else {
+                        self.getAll = true
+                        self.message = self.message + "\nGet all videos"
+                    }
+                    
+                case "-s":										// Nominate which sessions to download
+                    self.userSetSessionList = true
+                    if self.userSetGetAll {
+                        displaySyntaxError("*** You have set both the -a and -s options. These are mutually exclusive ***")
+                    } else {
+                        if i <= arguments.count {
+                            var sessions = ""
+                            var j = i
+                            while j < arguments.count && !arguments[j].hasPrefix("-") {
+                                sessions = sessions + arguments[j] as String
+                                j += 1
+                            }
+                            self.sessionIDs = sessions.componentsSeparatedByString(",")
+                            self.getAll = false
+                            self.message = self.message + "Downloading \(self.sessionIDs.count) sessions: \(self.sessionIDs)"
+                        } else {
+                            displaySyntaxError()
+                        }
+                    }
+                    
+                case "-sd":										// Get SD videos
+                    self.resolution = .sd
+                    self.message = self.message + "\nGet SD resolution"
+                    
+                case "-hd":										// Get HD videos
+                    self.resolution = .hd
+                    self.message = self.message + "\nGet HD resolution"
+                    
+                case "-nopdf":									// Don't get the associated PDFs
+                    self.getPDF = false
+                    self.message = self.message + "\nDon't get PDFs"
+                    
+                case "-pdfonly":								// Only download the PDFs (and not the videos)
+                    self.getPDF = true
+                    self.getVideo = false
+                    self.message = self.message + "\nOnly get PDFs"
+                    
+                case "-y":										// Nominate the year
+                    if i <= arguments.count {
+                        self.year = Int(arguments[i])!
+                        self.message = self.message + "\nGet downloads from \(self.year)"
+                        
+                    } else {
+                        displaySyntaxError()
+                    }
+                    
+                default:
                     displaySyntaxError()
                 }
-                
-            default:
-                displaySyntaxError()
             }
         }
+        print(self.message)
     }
-    print(message)
 }
 
-func displaySyntaxError(additionalMessage: String? = nil) {
-    print("usage: videoDownloader.swift [-d directory] [-a] [-s SessionID1, SessionID2...] [-hd | -sd] [-pdf-only] [-nopdf] [-y Year]\n")
-    if let message = additionalMessage {
-        print("\(message)\n")
-    }
-    print("OPTIONS\n")
-    print("-d        A directory to download the videos to. By default, the download goes in the current working directory")
-    print("-a        Download all the session videos (the default)")
-    print("-s        A list of one or more session IDs, separated by commas (e.g -s 100, 101, 102 etc)")
-    print("-hd | -sd Choose -hd for HD videos (the default) or -sd for SD videos")
-    print("-pdf-only Only get PDFs, i.e. do not download the videos")
-    print("-nopdf    Do not download the related PDFs")
-    print("-y        The year to use. The default is 2016. Use a four-digit year")
-    exit(0)
-}
-
-func validateURL(url : String) -> NSURL {
-    guard let baseURL = NSURL(string: url)
-        else {
-            print("Error: \(url) doesn't seem to be a valid URL")
-            exit(0)
-    }
-    return baseURL
-}
-
-func getHTMLPage(url: NSURL) -> String {
-    do {
-        let rawHTML = try String(contentsOfURL: url)
-        
-        return rawHTML
-    } catch let error as NSError {
-        print("Error: \(error)")
+extension Setup {
+    func displaySyntaxError(additionalMessage: String? = nil) {
+        print("usage: videoDownloader.swift [-d directory] [-a] [-s SessionID1, SessionID2...] [-hd | -sd] [-pdf-only] [-nopdf] [-y Year]\n")
+        if let message = additionalMessage {
+            print("\(message)\n")
+        }
+        print("OPTIONS\n")
+        print("-d        A directory to download the videos to. By default, the download goes in the current working directory")
+        print("-a        Download all the session videos (the default)")
+        print("-s        A list of one or more session IDs, separated by commas (e.g -s 100, 101, 102 etc)")
+        print("-hd | -sd Choose -hd for HD videos (the default) or -sd for SD videos")
+        print("-pdf-only Only get PDFs, i.e. do not download the videos")
+        print("-nopdf    Do not download the related PDFs")
+        print("-y        The year to use. The default is 2016. Use a four-digit year")
         exit(0)
     }
 }
 
-func getListOfAllSessions() -> [String] {
-    let baseString = "https://developer.apple.com/videos/wwdc\(year)/"
-    let baseURL = validateURL(baseString)
-    
-    let htmlPage = getHTMLPage(baseURL)
-    let regexString = "/videos/play/wwdc\(year)/([0-9]*)/"
-    
-    let result = extract(regexString, from: htmlPage)
-    print("Getting all \(result.count) sessions: \(result)")
-    return result
-}
+struct Web {
+    func validateURL(url : String) -> NSURL {
+        guard let baseURL = NSURL(string: url)
+            else {
+                print("Error: \(url) doesn't seem to be a valid URL")
+                exit(0)
+        }
+        return baseURL
+    }
 
-func extract(regularExpression: String, from text: String) -> [String] {
-    var returnArray : [String] = []
-    do {
-        let regex = try NSRegularExpression(pattern: regularExpression, options: [])
-        let matches = regex.matchesInString(text, options: [], range: NSMakeRange(0, text.characters.count))
-        var matchingResults : [String] = []
-        for match in matches {
-            if !NSEqualRanges(match.range, NSMakeRange(NSNotFound, 0)) {
-                let captureGroup = match.rangeAtIndex(1)
-                let matchedRange = text.rangeFromNSRange(captureGroup)
-                let matchedString = text.substringWithRange(matchedRange!)
-                matchingResults.append(matchedString)
+    func getHTMLPage(url: NSURL) -> String {
+        do {
+            let rawHTML = try String(contentsOfURL: url)
+            
+            return rawHTML
+        } catch let error as NSError {
+            print("Error: \(error)")
+            exit(0)
+        }
+    }
+
+    func getListOfAllSessions(setup : Setup) -> [String] {
+        let baseString = "https://developer.apple.com/videos/wwdc\(setup.year)/"
+        let baseURL = validateURL(baseString)
+        
+        let htmlPage = getHTMLPage(baseURL)
+        let regexString = "/videos/play/wwdc\(setup.year)/([0-9]*)/"
+        
+        let result = extract(regexString, from: htmlPage)
+        print("Getting all \(result.count) sessions: \(result)")
+        return result
+    }
+    
+    func getDownloads(setup : Setup) {
+        if setup.getAll {
+            setup.sessionIDs = getListOfAllSessions(setup)
+        }
+        if setup.getVideo {
+            for session in setup.sessionIDs {
+                let baseString = "https://developer.apple.com/videos/play/wwdc\(setup.year)/\(session)/"
+                let baseURL = validateURL(baseString)
+                
+                let htmlPage = getHTMLPage(baseURL)
+                let regexString = "<a\\ href=\\\"(.+)\\?dl=1\\\">\(setup.resolution.rawValue)\\ Video"
+                let urlString = extract(regexString, from: htmlPage)
+                let videoURL = validateURL(urlString[0])
+                
+                let fileLocation = setup.saveToDirectory
+                let fileLocationURL = validateURL(fileLocation)
+                
+                _ = Downloader().download(videoURL, to: fileLocationURL)
             }
         }
-        
-        let uniqueIDs = Array(Set(matchingResults))
-        returnArray = uniqueIDs.sort { $0 < $1 }
+    }
+}
+
+extension Web {
+    func extract(regularExpression: String, from text: String) -> [String] {
+        var returnArray : [String] = []
+        do {
+            let regex = try NSRegularExpression(pattern: regularExpression, options: [])
+            let matches = regex.matchesInString(text, options: [], range: NSMakeRange(0, text.characters.count))
+            var matchingResults : [String] = []
+            for match in matches {
+                if !NSEqualRanges(match.range, NSMakeRange(NSNotFound, 0)) {
+                    let captureGroup = match.rangeAtIndex(1)
+                    let matchedRange = text.rangeFromNSRange(captureGroup)
+                    let matchedString = text.substringWithRange(matchedRange!)
+                    matchingResults.append(matchedString)
+                }
+            }
+            
+            let uniqueIDs = Array(Set(matchingResults))
+            returnArray = uniqueIDs.sort { $0 < $1 }
+            return returnArray
+        } catch let error as NSError {
+            print("Regex error: \(error.localizedDescription)")
+        }
         return returnArray
-    } catch let error as NSError {
-        print("Regex error: \(error.localizedDescription)")
     }
-    return returnArray
 }
 
-func downloadSession(downloadURL : NSURL, toURL : NSURL) {
-        guard let dataFromURL = NSData(contentsOfURL: downloadURL)
-        else {
-            let error = NSError(domain:"Error downloading file", code:800, userInfo:nil)
-            completion(path: destinationUrl.path!, error: error)
-            return
+class Downloader : NSObject, NSURLSessionDownloadDelegate {
+    var url : NSURL?
+    var fileName : NSURL?
+    
+    override init()
+    {
+        super.init()
+    }
+    
+    //is called once the download is complete
+    func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didFinishDownloadingToURL location: NSURL) {
+        //copy downloaded data to your documents directory with same names as source file
+        let documentsURL =  NSFileManager.defaultManager().URLsForDirectory(.DocumentDirectory, inDomains: .UserDomainMask).first
+        let destinationUrl = documentsURL!.URLByAppendingPathComponent(fileName!.lastPathComponent!)
+        let dataFromURL = NSData(contentsOfURL: location)
+        dataFromURL!.writeToURL(destinationUrl, atomically: true)
+    }
+    
+    //this is to track progress
+    func URLSession(session: NSURLSession, downloadTask: NSURLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let percentage = Double(totalBytesWritten)/Double(totalBytesExpectedToWrite)
+        print("Downloaded \(totalBytesWritten) of \(totalBytesExpectedToWrite) (\(percentage)%)")
+    }
+    
+    // if there is an error during download this will be called
+    func URLSession(session: NSURLSession, task: NSURLSessionTask, didCompleteWithError error: NSError?) {
+        if(error != nil) {
+            //handle the error
+            print("Download interrupted with error: \(error!.localizedDescription)");
         }
+    }
+    
+    //method to be called to download
+    func download(url: NSURL, to fileName: NSURL) {
+        self.url = url
+        self.fileName = fileName
         
-        if dataFromURL.writeToURL(toURL, atomically: true) {
-            completion(path: destinationUrl.path!, error:nil)
-        } else {
-            let error = NSError(domain:"Error saving file", code:800, userInfo:nil)
-            completion(path: destinationUrl.path!, error:error)
-        }
-}
-
-func getDownloads() {
-    if getAll {
-        sessionIDs = getListOfAllSessions()
-    }
-    if getVideo {
-        for session in sessionIDs {
-            let baseString = "https://developer.apple.com/videos/play/wwdc\(year)/\(session)/"
-            let baseURL = validateURL(baseString)
-            
-            let htmlPage = getHTMLPage(baseURL)
-            let regexString = "<a\\ href=\\\"(.+)\\?dl=1\\\">\(resolution.rawValue)\\ Video"
-            let urlString = extract(regexString, from: htmlPage)
-            let downloadURL = validateURL(urlString[0])
-            
-            downloadSession(downloadURL, toURL: downloadURL)
-        }
+        //download identifier can be customized. I used the "ulr.absoluteString"
+        let sessionConfig = NSURLSessionConfiguration.backgroundSessionConfigurationWithIdentifier(url.absoluteString)
+        let session = NSURLSession(configuration: sessionConfig, delegate: self, delegateQueue: nil)
+        let task = session.downloadTaskWithURL(url)
+        task.resume()
     }
 }
 
 // MARK: Where the work is done
-processLaunchArguments()
-getDownloads()
+
+let app = Setup()
+Web().getDownloads(app)
 
